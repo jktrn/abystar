4afa7b80015e0e77cb583276b666e0ae3b9fc695
Jason <123jasont@gmail.com>
2023-05-18 15:56:12 -0700

    feat: utilize `genshin.dev` api

---
+++ src/utils/formatCharacterName.ts
@@ -0,0 +1,11 @@
+export function formatCharacterName(character: string) {
+    const formattedCharacter = character
+      .split('-')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+    if (formattedCharacter.startsWith('Traveler')) {
+      return formattedCharacter.replace(' ', ' (') + ')';
+    }
+    return formattedCharacter;
+  }
+  
