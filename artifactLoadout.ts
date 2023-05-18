929aff15e8405818c2bc97a44e0644686fb9b22a
vinal2 <prestons2523@gmail.com>
2023-05-17 20:39:44 -0700

    initial commit

---
+++ artifactLoadout.ts
@@ -0,0 +1,23 @@
+class artifactLoadout {
+    flower: artifact
+    feather: artifact
+    sands: artifact
+    cup: artifact
+    headpiece: artifact
+    bonusAtk: number
+    bonusDef: number
+    bonusHp: number
+    bonusElemDmg: number
+
+    constructor() {
+        this.flower = blankArtifact()
+        this.feather = blankArtifact()
+        this.sands = blankArtifact()
+        this.cup = blankArtifact()
+        this.headpiece = blankArtifact()
+        this.bonusAtk = 0
+        this.bonusDef = 0
+        this.bonusHp = 0
+        this.bonusElemDmg = 0
+    } // set each artifact individually later from user input and use artifact constructor there
+}
---
+++ stat.ts
@@ -0,0 +1,10 @@
+class stat {
+    substat: string
+    value: number
+
+    constructor(type: string, stat: number) { // when initializing 3 stat artifacts, pass null into both args
+        this.substat = type
+        this.value = stat
+    }
+
+}
