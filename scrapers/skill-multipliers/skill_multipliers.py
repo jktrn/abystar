d2ac5d7895a70f1a1adb659d521b80e8bae3d5cf
Jason <123jasont@gmail.com>
2023-05-20 22:18:28 -0700

    feat: fetch skills, bursts (`skill_multipliers.py`)

--- scrapers/skill-multipliers/skill_multipliers.py
+++ scrapers/skill-multipliers/skill_multipliers.py
@@ -4,7 +4,9 @@ import json
 from tqdm import tqdm
 
 def parse_cell(cell: str) -> str:
-    if '×' in cell:
+    if any(c.isalpha() for c in cell):
+        return cell
+    elif '×' in cell:
         value, multiplier = cell.split('×')
         value = float(value.strip().strip('%')) * float(multiplier)
         return f'{value:.2f}%'
@@ -26,29 +28,38 @@ def parse_row(row: list[str]) -> list[tuple[str, list[str]]]:
     else:
         return [(key, row[1:])]
 
-def fetch_character_data(character: str) -> dict[str, dict[str, str]]:
+def fetch_character_data(character: str) -> dict[str, dict[str, dict[str, str]]]:
     url = f'https://genshin.honeyhunterworld.com/{character}/'
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    table = soup.find('table', {'class': 'skill_dmg_table'})
+    tables = soup.find_all('table', {'class': 'skill_table'})
 
-    if table:
-        rows = table.find_all('tr')
-        header = [cell.text for cell in rows[0].find_all('td')[:16]]
-        data = [[parse_cell(cell.text) for cell in row.find_all('td')[:16]] for row in rows[1:]]
+    all_data = {}
 
-        data_dict = {}
-        for row in data:
-            parsed_rows = parse_row(row)
-            for key, values in parsed_rows:
-                data_dict[key] = {header[i]: values[i-1] for i in range(1, len(header))}
+    for table in tables:
+        skill_name_tag = table.find_all('td')[1].find('a')
+        
+        if skill_name_tag:
+            skill_name = skill_name_tag.text
+            skill_table = table.find('table', {'class': 'skill_dmg_table'})
+            
+            if skill_table:
+                rows = skill_table.find_all('tr')
+                header = [cell.text for cell in rows[0].find_all('td')[:16]]
+                data = [[parse_cell(cell.text) for cell in row.find_all('td')[:16]] for row in rows[1:]]
 
-        return data_dict
-    else:
-        return None
+                data_dict = {}
+                for row in data:
+                    parsed_rows = parse_row(row)
+                    for key, values in parsed_rows:
+                        data_dict[key] = {header[i]: values[i-1] for i in range(1, len(header))}
+                
+                all_data[skill_name] = data_dict
 
-def fetch_all_characters_data() -> dict[str, dict[str, dict[str, str]]]:
+    return all_data
+
+def fetch_all_characters_data() -> dict[str, dict[str, dict[str, dict[str, str]]]]:
     with open('../character-paths.json', 'r') as f:
         characters = json.load(f)
 
@@ -56,13 +67,13 @@ def fetch_all_characters_data() -> dict[str, dict[str, dict[str, str]]]:
 
     for character, name in tqdm(characters.items()):
         data_dict = fetch_character_data(character)
-        if data_dict is not None:
+        if data_dict:
             all_data[name] = data_dict
 
     return all_data
 
-def save_data_to_file(data: dict[str, dict[str, dict[str, str]]]) -> None:
-    with open('na_multipliers.json', 'w') as f:
+def save_data_to_file(data: dict[str, dict[str, dict[str, dict[str, str]]]]) -> None:
+    with open('skill-multipliers.json', 'w') as f:
         json.dump(data, f, indent=4)
 
 if __name__ == '__main__':
