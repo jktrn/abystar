5df318fa24b711b817740809c70c61e8c2b3c29e
Jason <123jasont@gmail.com>
2023-05-21 21:19:53 -0700

    feat: higher-quality fandom icons

--- data/characters/characters.py
+++ data/characters/characters.py
@@ -4,6 +4,7 @@ import json
 from tqdm import tqdm
 
 base_url = 'https://genshin.honeyhunterworld.com'
+icon_url = 'https://genshin-impact.fandom.com/wiki/Character'
 
 def parse_cell(cell: str) -> str:
     if any(c.isalpha() for c in cell):
@@ -43,7 +44,7 @@ def fetch_character_data(character: str) -> dict[str, dict[str, dict[str, str]]]
     data = {row.find_all('td')[0].text: row.find_all('td')[-1].text for row in rows[1:]}
 
     all_data["name"] = rows[0].find_all('td')[-1].text
-    all_data["image"] = base_url + rows[0].find('img')['src'].split('?')[0]
+    all_data["icon"] = None
     all_data["weapon"] = rows[5].find_all('td')[-1].text.strip()
     all_data["vision"] = data["Vision (Introduced)"]
     all_data["rarity"] = len(rows[4].find_all('img'))
@@ -87,7 +88,7 @@ def fetch_character_data(character: str) -> dict[str, dict[str, dict[str, str]]]
                     parsed_rows = parse_row(row)
                     for key, values in parsed_rows:
                         data_dict[key] = {header[i]: values[i-1] for i in range(1, len(header))}
-                
+
                 all_data["activeSkills"].append({
                     "name": skill_name,
                     "image": skill_image,
@@ -111,6 +112,20 @@ def fetch_character_data(character: str) -> dict[str, dict[str, dict[str, str]]]
 
     return all_data
 
+def fetch_icons():
+    # Fetch character icon from genshin-impact.fandom.com/wiki/Character and add it to the `icon` object
+    page_icon = requests.get(icon_url)
+    soup_icon = BeautifulSoup(page_icon.content, 'html.parser')
+    table_icon = soup_icon.find("table", { "class": "article-table" })
+    trs = table_icon.find_all("tr")
+    icons = {}
+    for tr in trs[1:]:
+        name = tr.find_all("td")[1].text.strip()
+        icon = tr.find("td").find("img")["data-src"].split("/revision")[0]
+        icons[name] = icon
+
+    return icons
+
 def fetch_all_characters_data() -> dict[str, dict[str, dict[str, dict[str, str]]]]:
     with open('paths/paths.json', 'r') as f:
         characters = json.load(f)
@@ -124,6 +139,11 @@ def fetch_all_characters_data() -> dict[str, dict[str, dict[str, dict[str, str]]
         if data_dict:
             all_data[name] = data_dict
 
+    icons = fetch_icons()
+    for name, data in all_data.items():
+        if name in icons:
+            data["icon"] = icons[name]
+
     return all_data
 
 def save_data_to_file(data: dict[str, dict[str, dict[str, dict[str, str]]]]) -> None:
@@ -131,10 +151,10 @@ def save_data_to_file(data: dict[str, dict[str, dict[str, dict[str, str]]]]) -> 
         json.dump(data, f, indent=4)
 
 if __name__ == '__main__':
-     # Comment this out if you want to test with a single character
-     all_data = fetch_all_characters_data()
-     save_data_to_file(all_data)
+    # Comment this out if you want to test with a single character
+    all_data = fetch_all_characters_data()
+    save_data_to_file(all_data)
 
-     # Uncomment these if you want to test a single character
-    #  ayaka_data = fetch_character_data('ayaka')
-    #  print(json.dumps(ayaka_data, indent=4))
+    # Uncomment these if you want to test a single character
+    # ayaka_data = fetch_character_data('ayaka')
+    # print(json.dumps(ayaka_data, indent=4))
