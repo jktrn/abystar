4721eefb0f20dd8031acafa4454dfab5dc933e00
Jason <123jasont@gmail.com>
2023-05-22 22:25:22 -0700

    chore: lint root configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-    colors: {
-        main: {
-            500: '#353535',
-            600: '#303030',
-            700: '#1D1D1D',
-            800: '#171717',
-            900: '#111111',
-            1000: '#0C0C0C',
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
         },
-    }
-  },
-  plugins: [],
+        colors: {
+            main: {
+                500: '#353535',
+                600: '#303030',
+                700: '#1D1D1D',
+                800: '#171717',
+                900: '#111111',
+                1000: '#0C0C0C',
+            },
+        }
+    },
+    plugins: [],
 }
