cba3a2e67793cc2aed89031acd5b91960027f328
Jason <123jasont@gmail.com>
2023-05-27 18:04:41 -0700

    refactor!: use `wanderer.moe` skill icons

--- src/components/ActiveSkillsSelect.tsx
+++ src/components/ActiveSkillsSelect.tsx
@@ -6,6 +6,13 @@ import elementColors from '@/utils/elementColors'
 
 const activeSkillOptions: any[] = []
 
+function kebabCase(str: string) {
+    return str
+        .replace(/([a-z])([A-Z])/g, '$1-$2')
+        .replace(/\s+/g, '-')
+        .toLowerCase()
+}
+
 // TODO: There has got to be a better way of doing this
 for (let i = 1; i <= 15; i++) {
     activeSkillOptions.push({ value: i, label: `Lv${i}` })
@@ -20,19 +27,26 @@ function ActiveSkillsSelect({
     activeSkills: string[]
     setActiveSkills: (value: string[]) => void
 }) {
+    //TODO: Implement this into the JSON
+    const skillIcons = [
+        `/images/skill-icons/normal-attacks/${kebabCase(character.weapon)}.png`,
+        `/images/skill-icons/${kebabCase(character.name)}-skill.png`,
+        `/images/skill-icons/${kebabCase(character.name)}-burst.png`,
+    ]    
+
     return (
         <div className="ml-4 flex flex-col justify-end gap-2">
             {character.activeSkills.map((skill, index) => (
                 <div key={skill.name} className="flex items-center gap-2">
                     <Image
-                        src={skill.image}
+                        src={skillIcons[index]}
                         alt={skill.name}
-                        bg={
-                            elementColors[
-                                character.vision.toLowerCase() as keyof typeof elementColors
-                            ]
-                        }
-                        className="!h-8 !w-10 overflow-hidden rounded-full object-cover p-1"
+                        // bg={
+                        //     elementColors[
+                        //         character.vision.toLowerCase() as keyof typeof elementColors
+                        //     ]
+                        // }
+                        className="!h-8 !w-10 bg-main-700 overflow-hidden rounded-full object-cover p-1"
                     />
                     <Select
                         instanceId={`active-skill-${index}-select`}
