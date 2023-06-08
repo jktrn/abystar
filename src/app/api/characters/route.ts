8ede66e3521cee7bfc85c9d6df79fc8c8dc1ff43
Jason <123jasont@gmail.com>
2023-06-08 15:22:18 -0700

    fix: bad import breaks build

--- src/app/api/characters/route.ts
+++ src/app/api/characters/route.ts
@@ -1,6 +1,6 @@
-import characters from '@/data/characters/characters.json'
+import { characterData } from '@/data'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-    return NextResponse.json(characters)
+    return NextResponse.json(characterData)
 }
