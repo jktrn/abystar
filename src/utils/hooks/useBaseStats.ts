51a8b1fb74a50c3092da97ceea5c881b113b5daf
flocto <57694588+flocto@users.noreply.github.com>
2023-06-15 14:52:10 -0500

    fix: dependency list for bonus application (#21)

--- src/utils/hooks/useBaseStats.ts
+++ src/utils/hooks/useBaseStats.ts
@@ -2,6 +2,16 @@ import { Bonus, Character } from '@/types/Character'
 import { convertBaseStats } from '@/utils'
 import { useMemo } from 'react'
 
+function findDiff(obj1: any, obj2: any) {
+    const diff: any = {}
+    for (const key in obj1) {
+        if (obj1[key] !== obj2[key]) {
+            diff[key] = [obj1[key], obj2[key]]
+        }
+    }
+    return diff
+}
+
 export default function useBaseStats(
     character: Character,
     characterLevel: string,
@@ -19,9 +29,13 @@ export default function useBaseStats(
     let newActiveSkills = [...activeSkills]
     let hasChanged = true
 
-    while (hasChanged) {
+    let maxIterations = 15
+    let appliedBonuses: Bonus[] = [...activeBonuses, ...activeConstellations]
+    let redoBonuses: Bonus[] = []
+    let dependencyMap: { [key: string]: Bonus[] } = {}
+    while (hasChanged && maxIterations--) {
         hasChanged = false
-        for (const bonus of [...activeBonuses, ...activeConstellations]) {
+        for (const bonus of appliedBonuses) {
             const oldBaseStats = { ...newBaseStats }
             newBaseStats = bonus.effect(
                 newBaseStats,
@@ -29,12 +43,33 @@ export default function useBaseStats(
                 newActiveSkills,
                 initialBaseStats
             )
+            if (bonus.dependencies) {
+                for (const dependency of bonus.dependencies) {
+                    if (!dependencyMap[dependency]) {
+                        dependencyMap[dependency] = []
+                    }
+                    dependencyMap[dependency].push(bonus)
+                }
+            }
+            let diff = findDiff(oldBaseStats, newBaseStats)
+            console.log(bonus.name, JSON.stringify(oldBaseStats) !== JSON.stringify(newBaseStats))
+            for (const key in diff) {
+                console.log(key, diff[key])
+                if (dependencyMap[key]) {
+                    redoBonuses.push(...dependencyMap[key])
+                    delete dependencyMap[key]
+                }
+            }
+
             if (JSON.stringify(oldBaseStats) !== JSON.stringify(newBaseStats)) {
                 hasChanged = true
             }
         }
+        appliedBonuses = redoBonuses
     }
-
+    if (maxIterations <= 0) {
+        throw new Error("Max iterations reached")
+    }
     return { baseStats: newBaseStats, updatedActiveSkills: newActiveSkills }
 }
 
