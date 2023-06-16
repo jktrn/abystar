802e43c4c5f8a0f8457bf96b06ef426cc7f8210a
flocto <57694588+flocto@users.noreply.github.com>
2023-06-16 17:04:48 -0500

    refactor: consolidate utility into `utils.ts` (#22)

--- src/utils/damage/parseScalingValue.ts
+++
@@ -1,7 +0,0 @@
-const parseScalingValue = (scalingValue: string) => {
-    const values = scalingValue.match(/\d+(\.\d+)?/g)
-    const parsedValues = values ? values.map((value) => parseFloat(value)) : [0]
-    return parsedValues
-}
-
-export default parseScalingValue
