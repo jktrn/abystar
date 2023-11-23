8110a10bb7f55721d723c5ee25694877f8913c70
jason <123jasont@gmail.com>
2023-11-22 16:20:29 -0800

    chore: init

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false,
+    trailingComma: 'es5',
+    singleQuote: true,
+    printWidth: 85,
+    tabWidth: 4,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    plugins: ['prettier-plugin-tailwindcss'],
+}
