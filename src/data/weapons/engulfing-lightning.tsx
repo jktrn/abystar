c8bf667f04e639c6b984728278ae94b18feb3315
nilocnus <colinjahuasun@gmail.com>
2023-12-27 00:57:25 -0800

    feat: implemented engulfing-lightning

--- src/data/weapons/engulfing-lightning.tsx
+++ src/data/weapons/engulfing-lightning.tsx
@@ -1,9 +1,36 @@
 import { Weapon } from '@/interfaces/Weapon'
-// import { Bonus } from '@/interfaces/Character'
+import { Bonus } from '@/interfaces/Character'
 
-// const weaponBonuses: Bonus[] = [
-// TODO: Implement
-// ]
+const weaponBonuses: Bonus[] = [
+    {
+        name: 'Timeless Dream: Eternal Stove',
+        effect: (attributes, talentLevels, currentStacks, state) => {
+            if (!currentStacks || !state || !state.weaponRefinement) {
+                return { attributes }
+            }
+
+            const energyRechargetoAttackBonus = [0.28, 0.35, 0.42, 0.49, 0.56]
+            const energyRechargetoAttackMax = [0.8, 0.9, 1.0, 1.1, 1.2]
+            const burstEnergyRechargeBonus = [0.3, 0.35, 0.4, 0.45, 0.5]
+
+            let hasBurst = 0
+
+            if(currentStacks === 2) {
+                hasBurst = burstEnergyRechargeBonus[state.weaponRefinement - 1]
+            }
+
+            const newAttributes = {
+                ...attributes,
+                ATK: attributes['ATK' || 0] * (1 + Math.min(((attributes['Energy Recharge'] || 0) - 1 + hasBurst) * energyRechargetoAttackBonus[state.weaponRefinement - 1], energyRechargetoAttackMax[state.weaponRefinement - 1])),
+                'Energy Recharge': attributes['Energy Recharge' || 0] + hasBurst
+            }
+            return { attributes: newAttributes }
+        },
+        maxStacks: 2,
+        stackOptions: ['Off', 'Enabled', 'Enabled + Burst'],
+        priority: 2
+    }
+]
 
 const EngulfingLightning: Weapon = {
     name: 'Engulfing Lightning',
@@ -76,31 +103,56 @@ const EngulfingLightning: Weapon = {
     refinements: [
         {
             description:
-                'ATK increased by 28% of Energy Recharge over the base 100%. You can gain a maximum bonus of 80% ATK. Gain 30% Energy Recharge for 12s after using an Elemental Burst.',
+                <span>
+                    ATK increased by <span style={{ color: '#ddd' }}>28%</span> of Energy Recharge over the base 100%. 
+                    You can gain a maximum bonus of <span style={{ color: '#ddd' }}>80%</span> ATK.
+                    Gain <span style={{ color: '#ddd' }}>30%</span> Energy Recharge for <span style={{ color: '#ddd' }}>12s</span> after using an Elemental Burst.
+                </span>
+                ,
             level: 1,
         },
         {
             description:
-                'ATK increased by 35% of Energy Recharge over the base 100%. You can gain a maximum bonus of 90% ATK. Gain 35% Energy Recharge for 12s after using an Elemental Burst.',
+                <span>
+                    ATK increased by <span style={{ color: '#ddd' }}>35%</span> of Energy Recharge over the base 100%. 
+                    You can gain a maximum bonus of <span style={{ color: '#ddd' }}>90%</span> ATK.
+                    Gain <span style={{ color: '#ddd' }}>35%</span> Energy Recharge for <span style={{ color: '#ddd' }}>12s</span> after using an Elemental Burst.
+                </span>
+                ,
             level: 2,
         },
         {
             description:
-                'ATK increased by 42% of Energy Recharge over the base 100%. You can gain a maximum bonus of 100% ATK. Gain 40% Energy Recharge for 12s after using an Elemental Burst.',
+                <span>
+                    ATK increased by <span style={{ color: '#ddd' }}>42%</span> of Energy Recharge over the base 100%. 
+                    You can gain a maximum bonus of <span style={{ color: '#ddd' }}>100%</span> ATK.
+                    Gain <span style={{ color: '#ddd' }}>40%</span> Energy Recharge for <span style={{ color: '#ddd' }}>12s</span> after using an Elemental Burst.
+                </span>
+                ,
             level: 3,
         },
         {
             description:
-                'ATK increased by 49% of Energy Recharge over the base 100%. You can gain a maximum bonus of 110% ATK. Gain 45% Energy Recharge for 12s after using an Elemental Burst.',
+                <span>
+                    ATK increased by <span style={{ color: '#ddd' }}>49%</span> of Energy Recharge over the base 100%. 
+                    You can gain a maximum bonus of <span style={{ color: '#ddd' }}>110%</span> ATK.
+                    Gain <span style={{ color: '#ddd' }}>45%</span> Energy Recharge for <span style={{ color: '#ddd' }}>12s</span> after using an Elemental Burst.
+                </span>
+                ,
             level: 4,
         },
         {
             description:
-                'ATK increased by 56% of Energy Recharge over the base 100%. You can gain a maximum bonus of 120% ATK. Gain 50% Energy Recharge for 12s after using an Elemental Burst.',
+                <span>
+                    ATK increased by <span style={{ color: '#ddd' }}>56%</span> of Energy Recharge over the base 100%. 
+                    You can gain a maximum bonus of <span style={{ color: '#ddd' }}>120%</span> ATK.
+                    Gain <span style={{ color: '#ddd' }}>50%</span> Energy Recharge for <span style={{ color: '#ddd' }}>12s</span> after using an Elemental Burst.
+                </span>
+                ,
             level: 5,
         },
     ],
-    // weaponBonuses
+    weaponBonuses
 }
 
 export default EngulfingLightning
